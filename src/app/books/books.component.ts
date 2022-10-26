f7a68253a5288f5f87ce95a32110db528fc837b0
Shijo Shaji <jojovms@gmail.com>
2022-10-26 12:59:01 +0530

    pipes

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -5,6 +5,7 @@ interface Books {
   name: string,
   author: string,
   imgUrl: string;
+  amount: number;
 }
 
 
@@ -24,16 +25,19 @@ export class BooksComponent implements OnInit {
       name: 'Do It Today',
       author: 'Darius Foroux',
       imgUrl: "https://m.media-amazon.com/images/I/71gfBC95RIL._AC_UY327_FMwebp_QL65_.jpg",
+      amount: 259.00
     },
     {
       name: 'The Psychology of Money',
       author: 'Morgan Housel',
       imgUrl: "https://d2i0w0hu6hvxgc.cloudfront.net/B08FHZ5L47/648546d0/cover.jpeg",
+      amount: 559.00
     },
     {
       name: 'Rich Dad Poor Dad',
       author: 'Robert T. Kiyosaki',
       imgUrl: "https://d2i0w0hu6hvxgc.cloudfront.net/B07C7M8SX9/4dc0769a/cover.jpeg",
+      amount: 959.80
     }
   ];
   isDisplayed: boolean = false;
