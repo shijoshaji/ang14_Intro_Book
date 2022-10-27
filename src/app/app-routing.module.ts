0a0e64137f3e086633a16bc4c456263b619f2f13
Shijo Shaji <jojovms@gmail.com>
2022-10-27 14:09:33 +0530

    created 2 new component: Signup & Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 import { BooksComponent } from './books/books.component';
 import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
-  {path:'', component:BooksComponent},
-  {path:'cart', component:CartComponent}
+  { path: '', component: BooksComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent }
 ];
 
 @NgModule({
